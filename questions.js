ca62e252a938d82ac201ceee816acef3cefca162
Rohit Kushwaha <64749971+rohitkush-waha@users.noreply.github.com>
2021-11-15 12:36:15 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+  numb: 1,
+  question: "HTML stands for -",
+  answer: "HyperText Markup Language",
+  options: [
+    "HighText Machine Language",
+    "HyperText and links Markup Language",
+    "HyperText Markup Language",
+    "None of these"
+  ]
+},
+  {
+  numb: 2,
+  question: "CSS stands for -",
+  answer: "Cascading style sheets",
+  options: [
+    "Cascade style sheets",
+    "Color and style sheets",
+    "Cascading style sheets",
+    "None of the above"
+  ]
+},
+  {
+  numb: 3,
+  question: "Which type of JavaScript language is ___",
+  answer: "Object-Based",
+  options: [
+    "Object-Oriented",
+    "Object-Based",
+    "Assembly-language",
+    "High-level"
+  ]
+},
+  {
+  numb: 4,
+  question: "SQL stand for?",
+  answer: "Structure Query Language",
+  options: [
+    "Structured Query List",
+    "Structure Query Language",
+    "Sample Query Language",
+    "None of the above"
+  ]
+},
+  {
+  numb: 5,
+  question: "Who invented Java Programming",
+  answer: "James Gosling",
+  options: [
+    "Guido van Rossum",
+    "James Gosling",
+    "Dennis Ritchie",
+    "Bjarne Stroustrup"
+  ]
+},
+];
